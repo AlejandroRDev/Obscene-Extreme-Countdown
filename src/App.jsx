9f1c9d6dd2c9b992741b625a3b5d940e8eae5e71
AlejandroRDev <iron-man@avengers.com>
2022-02-01 17:36:54 +0100

    Obscene Extreme Clocks without CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import DigitalClock from './components/DigitalClock/DigitalClock';
+import Countdown from './components/Countdown/Countdown';
+import Stopwatch from './components/Stopwatch/Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+     <DigitalClock />
+     <Countdown />
+     <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
